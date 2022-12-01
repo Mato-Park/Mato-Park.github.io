27bd4c97380c1f760ba91ea299f4a31e7bee2940
Mato-Park <dark1432@naver.com>
2022-12-01 21:46:07 +0900

    첫 포스팅 오타 수정

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,1 +1,7 @@
-var store = []
+var store = [{
+        "title": "첫 포스팅 해기!",
+        "excerpt":"GitHub Blog를 활용한 첫 테스트 포스팅이다. 데이터 분석/개발/IT 쪽 관련 내용을 주로 포스팅할 예정이며, 가끔 일상 관련된 내용도 올리고자 한다.   내가 갖고 있는 지식과 스킬을 나누고 공유하여 모두 윈윈할 수 있는 그런 블로그가 되면 좋겠다. 소통의 장이랄까? 그런 블로그가 되길 원한다.  ","categories": ["Blog"],
+        "tags": ["Blog"],
+        "url": "/blog/first-post-test/",
+        "teaser": null
+      }]
